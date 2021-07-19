252ca80bc5b1a281ed041844a1ae7f20f024bbb6
vlad-oleynik <vlad@stardevs.gg>
2021-07-19 23:19:48 +0300

    Code Coverage and Quality

--- project/app/main.py
+++ project/app/main.py
@@ -1,15 +1,35 @@
-from fastapi import FastAPI, Depends
-
-from app.config import get_settings, Settings
+# project/app/main.py
 
 
-app = FastAPI()
+import logging
+
+from fastapi import FastAPI
+
+from app.api import ping, summaries  # updated
+from app.db import init_db
+
+log = logging.getLogger("uvicorn")
 
 
-@app.get("/ping")
-async def pong(settings: Settings = Depends(get_settings)):
-    return {
-        "ping": "pong!",
-        "environment": settings.environment,
-        "testing": settings.testing
-    }
+def create_application() -> FastAPI:
+    application = FastAPI()
+    application.include_router(ping.router)
+    application.include_router(
+        summaries.router, prefix="/summaries", tags=["summaries"]
+    )  # new
+
+    return application
+
+
+app = create_application()
+
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
